6b80725bc101df70f44cc0e81867e1bcef730942
HadilAbdennebi <77753409+HadilAbdennebi@users.noreply.github.com>
2021-04-14 14:33:48 +0200

    Add files via upload

---
+++ Produit/src/main/java/com/hadil/produits/service/ProduitService.java
@@ -0,0 +1,16 @@
+package com.hadil.produits.service;
+
+import java.util.List;
+
+import com.hadil.produits.entities.Produit;
+
+public interface ProduitService {
+	
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	 void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+
+}
